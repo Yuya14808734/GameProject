9eb7b5754737888eff1a84c312082a67c3346902
Yuya14808734 <101414725+Yuya14808734@users.noreply.github.com>
2024-02-09 00:13:23 +0900

    ,エフェクト追加中

--- DX22Base/Effect01_SingleSmoke.cpp
+++ DX22Base/Effect01_SingleSmoke.cpp
@@ -2,6 +2,10 @@
 
 SingleSmoke::SingleSmoke()
 {
+	m_SmokeImage.SetTexture("Assets/Texture/Cloud00.png");
+	m_SmokeImage.SetSize(CVector2(256.0f, 256.0f));
+	SetPos(CVector3::GetZero());
+	SetTime(1.0f);
 }
 
 SingleSmoke::~SingleSmoke()
@@ -10,12 +14,29 @@ SingleSmoke::~SingleSmoke()
 
 void SingleSmoke::Update()
 {
+	m_Time += 1.0f / 60.0f;
+
+	if (m_Time > m_EndTime)
+	{
+		m_isDestroy = true;
+	}
 }
 
 void SingleSmoke::Draw()
 {
+	m_SmokeImage.Draw();
 }
 
 void SingleSmoke::PausedEffect(bool paused)
 {
+}
+
+void SingleSmoke::SetPos(const CVector3& pos)
+{
+	m_SmokeImage.m_BasePos = pos;
+}
+
+void SingleSmoke::SetTime(float EndTime)
+{
+	m_EndTime = EndTime;
 }
--- DX22Base/Effect01_SingleSmoke.h
+++ DX22Base/Effect01_SingleSmoke.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "EffectBase.h"
 #include "Calculation.h"
+#include "Image3D.h"
 
 
 class SingleSmoke : public EffectBase
@@ -11,7 +12,11 @@ public:
 	void Update() override;
 	void Draw() override;
 	void PausedEffect(bool paused = true) override;
+	void SetPos(const CVector3& pos);
+	void SetTime(float EndTime);
 
 private:
-
+	Image3D m_SmokeImage;
+	float m_Time = 0.0f;
+	float m_EndTime = 0.0f;
 };
